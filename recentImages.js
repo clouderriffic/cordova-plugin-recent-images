b823f43631641fa42c1dd6fb0e72dc6df2fa2d02
voomik <voomik@Pablos-MacBook-Pro.local>
2015-09-30 23:31:16 -0500

    Initial commit

---
+++ recentImages.js
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+/*global cordova,window,console*/
+/**
+ * An Image Picker plugin for Cordova
+ * 
+ * Developed by Wymsee for Sync OnSet
+ */
+
+var RecentImages = function() {
+
+};
+
+/*
+*	success - success callback
+*	fail - error callback
+*	options
+*		.maximumImagesCount - max images to be selected, defaults to 15. If this is set to 1, 
+*		                      upon selection of a single image, the plugin will return it.
+*		.width - width to resize image to (if one of height/width is 0, will resize to fit the
+*		         other while keeping aspect ratio, if both height and width are 0, the full size
+*		         image will be returned)
+*		.height - height to resize image to
+*		.quality - quality of resized image, defaults to 100
+*/
+RecentImages.prototype.getRecentImages = function(success, fail, options) {
+	if (!options) {
+		options = {};
+	}
+	
+	var params = {
+		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 10,
+		width: options.width ? options.width : 0,
+		height: options.height ? options.height : 0,
+		quality: options.quality ? options.quality : 1.0
+	};
+
+	return cordova.exec(success, fail, "CTRecentImagesManager", "getRecentImages", [params]);
+};
+
+window.recentImages = new RecentImages();
---
+++ src/ios/CTRecentImages.m
@@ -0,0 +1,144 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+#import "CTRecentImages.h"
+@import Photos;
+
+@implementation CTRecentImages
+
+- (instancetype)init {
+    if (!self.imagesArray){
+        self.imagesArray = [[NSMutableArray alloc] init];
+    }
+    return [super init];
+}
+
+- (void)fetchRecentPhotosWithImageOptions:(NSDictionary *)imageOptions completion:(void (^)(NSArray *images))completion {
+    self.maximumImagesCount = [[imageOptions objectForKey:@"maximumImagesCount"] integerValue];
+    self.width = [[imageOptions objectForKey:@"width"] integerValue];
+    self.height = [[imageOptions objectForKey:@"height"] integerValue];
+    self.quality = [[imageOptions objectForKey:@"quality"] floatValue];
+    [self checkGalleryPermissions:^(BOOL granted) {
+        if(granted) {
+            [self fetchRecentPhotosWithIndexCount:0 completion:completion];
+
+        }
+    }];
+}
+- (void)fetchRecentPhotosWithIndexCount:(int)indexCount completion:(void (^)(NSArray *images))completion {
+    
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    options.synchronous = YES;
+    options.networkAccessAllowed = YES;
+    PHFetchOptions *fetchOptions = [[PHFetchOptions alloc]init];
+    fetchOptions.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:YES]];
+    fetchOptions.fetchLimit = self.maximumImagesCount;
+    
+    PHFetchResult *photos = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:fetchOptions];
+    
+    if (photos) {
+        [[PHImageManager defaultManager] requestImageForAsset:[photos objectAtIndex:photos.count -1 -indexCount] targetSize:CGSizeMake(self.width, self.height) contentMode:PHImageContentModeAspectFill options:options resultHandler:^(UIImage *result, NSDictionary *info) {
+            UIImage *scaledImage = [self imageByScalingNotCroppingForSize:result toSize:CGSizeMake(self.width, self.height)];
+            NSString *encodedImage = [self encodeToBase64String:scaledImage];
+            [self.imagesArray addObject:encodedImage];
+            
+            if (indexCount + 1 < photos.count && self.imagesArray.count < 10) {
+                [self fetchRecentPhotosWithIndexCount:indexCount +1 completion:completion];
+            } else {
+                completion([self.imagesArray copy]);
+            }
+        }];
+    }
+}
+- (NSString *)encodeToBase64String:(UIImage *)image {
+    return[UIImageJPEGRepresentation(image, self.quality) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+}
+- (UIImage*)imageByScalingNotCroppingForSize:(UIImage*)anImage toSize:(CGSize)frameSize
+{
+    UIImage* sourceImage = anImage;
+    UIImage* newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = frameSize.width;
+    CGFloat targetHeight = frameSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGSize scaledSize = frameSize;
+    
+    if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        // opposite comparison to imageByScalingAndCroppingForSize in order to contain the image within the given bounds
+        if (widthFactor == 0.0) {
+            scaleFactor = heightFactor;
+        } else if (heightFactor == 0.0) {
+            scaleFactor = widthFactor;
+        } else if (widthFactor > heightFactor) {
+            scaleFactor = heightFactor; // scale to fit height
+        } else {
+            scaleFactor = widthFactor; // scale to fit width
+        }
+        scaledSize = CGSizeMake(width * scaleFactor, height * scaleFactor);
+    }
+    
+    UIGraphicsBeginImageContext(scaledSize); // this will resize
+    
+    [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if (newImage == nil) {
+        NSLog(@"could not scale image");
+    }
+    
+    // pop the context to get back to the default
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (void)checkGalleryPermissions:(void (^)(BOOL granted))block
+{
+    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    
+    switch (status)
+    {
+        case PHAuthorizationStatusAuthorized:
+            block(YES);
+            break;
+        case PHAuthorizationStatusNotDetermined:
+        {
+            [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus authorizationStatus)
+             {
+                 if (authorizationStatus == PHAuthorizationStatusAuthorized)
+                 {
+                     block(YES);
+                 }
+                 else
+                 {
+                     block(NO);
+                 }
+             }];
+            break;
+        }
+        default:
+            block(NO);
+            break;
+    }
+}
+
+@end
